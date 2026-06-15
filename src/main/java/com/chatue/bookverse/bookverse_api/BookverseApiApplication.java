63640ecca855dfa49754f7460789e400fa3c1ec1
chatue leslie <chatueleslie427@gmail.com>
2026-06-15 23:17:41 +0200

    Initial commit

---
+++ src/main/java/com/chatue/bookverse/bookverse_api/BookverseApiApplication.java
@@ -0,0 +1,20 @@
+package com.chatue.bookverse.bookverse_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class BookverseApiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BookverseApiApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookverseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatue/bookverse/bookverse_api/BookverseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatue.bookverse.bookverse_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookverseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
